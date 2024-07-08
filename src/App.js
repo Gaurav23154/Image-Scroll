c48bb39ba5d7b77a47771e303dad47626dcac1a8
Gaurav Jaiswal <gauravjaiswal5200@gmail.com>
2024-07-08 14:28:14 +0530

    React Practice Image scroller

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Images from './Images';
 import './App.css';
 
 function App() {
+   
+   const [currentImages,setCurrentImages]=useState(0);
+  
+   const handleOnRight=()=>{
+    if(currentImages < Images.length-1){
+      console.log(currentImages);
+      setCurrentImages(currentImages+1);
+    }
+    else{console.log(currentImages);
+      setCurrentImages(0);
+      
+    }
+   }
+   const handleOnLeft=()=>{
+    if(currentImages===0){console.log(currentImages);
+       setCurrentImages(Images.length-1);
+       
+    }
+    else{console.log(currentImages);
+      setCurrentImages(currentImages-1);
+      
+    }
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <button onClick={handleOnLeft}>left</button>
+    {/* <div className='button-container' > */}
+      <img src={Images[currentImages]} style={{height:"500px" ,width:"500px"}}  alt='images'></img>
+    {/* </div> */}
+    <button onClick={handleOnRight}>right</button>
     </div>
   );
 }
